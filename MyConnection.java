07d212cb0b301cc28bdaeb4f950d1b8195f78740
rimeneguediche <92642950+rimeneguediche@users.noreply.github.com>
2022-10-27 10:32:35 +0100

    utils

---
+++ MyConnection.java
@@ -0,0 +1,39 @@
+package com.example.newagence.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static MyConnection instance;
+
+    public String url="jdbc:mysql://localhost:3306/rimene";
+    public String login="root";
+    public String pwd="";
+    Connection cnx ;
+
+    public MyConnection() {
+
+        try {
+            cnx = DriverManager.getConnection(url,login,pwd);
+            System.out.println("Connexion made!");
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public static MyConnection getInstance() {
+        if (instance == null)
+        {
+            instance = new MyConnection();
+        }
+        return instance;
+
+    }
+
+
+    public Connection getCnx (){
+        return cnx;
+    }}
+
